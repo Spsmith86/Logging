c6ea4d379b5a9ccd2095546b395512c5e4458021
Sean Smith <ssmith468@gatech.edu>
2020-08-25 20:25:57 -0700

    Added unit tests for Log method

--- LoggingTests/FileSystemLoggerTests.cs
+++ LoggingTests/FileSystemLoggerTests.cs
@@ -1,6 +1,7 @@
 using Logging;
 using Moq;
 using System;
+using System.IO;
 using Xunit;
 
 namespace LoggingTests
@@ -28,7 +29,42 @@ namespace LoggingTests
 			fileSystem.Setup(e => e.DirectoryExists(It.IsAny<string>())).Returns(false);
 
 			ILogMessageFormatter logMessageFormatter = new DefaultLogMessageFormatter();
+
 			Assert.Throws<ArgumentException>(() => new FileSystemLogger(fileSystem.Object, logMessageFormatter, "C:/Logs", "Log.log"));
+		}
+
+		[Fact]
+		public void Log_NoMessage_ThrowsArgumentException()
+		{
+			Mock<IFileSystem> fileSystem = new Mock<IFileSystem>();
+			fileSystem.Setup(e => e.DirectoryExists(It.IsAny<string>())).Returns(true);
+
+			ILogMessageFormatter logMessageFormatter = new DefaultLogMessageFormatter();
+			ILogger logger = new FileSystemLogger(fileSystem.Object, logMessageFormatter, "C:/Logs", "Log.log");
+			LogMessage logMessage = new LogMessage(LogLevel.DEBUG, String.Empty);
+
+			Assert.Throws<ArgumentException>(() => logger.Log(logMessage));
+		}
+
+		[Fact]
+		public void Log_CallsWriteToFile()
+		{
+			string directoryPath = "C:/Logs";
+			string fileName = "Log.log";
+			string combined = Path.Combine(directoryPath, fileName);
+			Mock<IFileSystem> fileSystem = new Mock<IFileSystem>();
+			fileSystem.Setup(e => e.DirectoryExists(It.IsAny<string>())).Returns(true);
+			fileSystem.Setup(e => e.WriteToFile(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>()));
+			fileSystem.Setup(e => e.CombineDirectoryPathAndFileName(directoryPath, fileName)).Returns(combined);
+
+			ILogMessageFormatter logMessageFormatter = new DefaultLogMessageFormatter();
+			ILogger logger = new FileSystemLogger(fileSystem.Object, logMessageFormatter, directoryPath, fileName);
+			LogMessage logMessage = new LogMessage(LogLevel.DEBUG, "Some Message", DateTime.Now, Guid.NewGuid());
+
+			logger.Log(logMessage);
+
+			string formattedMessage = logMessageFormatter.FormatLogMessage(logMessage);
+			fileSystem.Verify(mock => mock.WriteToFile(combined, formattedMessage, true), Times.Once());
 		}
 	}
 }
